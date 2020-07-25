3a0ce3d11e357b8612d9dd56ce10f8332995ff4b
J C <jc@Js-MacBook-Pro.fios-router.home>
2020-07-25 04:12:23 -0400

    new commit

--- sensor.py
+++ sensor.py
@@ -15,13 +15,12 @@ class Sensor:
         cond_and_temp = ''
         while 'Conductivity:' not in cond_and_temp and 'Temperature:' not in cond_and_temp:
             try:
-                self.ser.write(bytes('do sample','utf-8')) #take sample immediately upon running code
+                self.ser.write(bytes('do sample','utf-8'))
                 self.ser.write(bytes('\r\n','utf-8'))
                 self.ser_bytes = self.ser.readline()
                 cond_and_temp = ' '.join(self.ser_bytes[:-2].decode('utf-8').strip().split()[-4:]) + '\n'
             except:
                 print('failed do_sample')
-
         with open('cond_and_temp.txt', 'a') as f:
                 f.write('Conductivity: {} Temperature: {}\n'.format(cond_and_temp.split()[1], cond_and_temp.split()[3]))
     def close(self):
