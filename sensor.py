ba1b45ea00944e49d11980eb5604455baf328d82
J C <jc@Js-MacBook-Pro.fios-router.home>
2020-07-31 19:52:58 -0400

    new commit

--- sensor.py
+++ sensor.py
@@ -90,14 +90,13 @@ class OxygenSensor(Sensor):
 		failed_conductivity = True
 		while time.time() <= end_at and self.written_samples < self.n_samples:
 			while 'Oxygen:' not in sat_and_temp and 'Saturation:' not in sat_and_temp and 'Temperature:' not in sat_and_temp:
-				print(sat_and_temp)
 				try:
 					self.ser.flushInput()
 					self.ser.write(bytes('do sample','utf-8'))
 					self.ser.write(bytes('\r\n','utf-8'))
 					self.ser_bytes = self.ser.readline()
 					sat_and_temp = ' '.join(self.ser_bytes.decode('utf-8').strip().split()) + '\n'
-					print(sat_and_temp)
+					print(sat_and_temp.split()[:-5])
 					failed_conductivity = False
 					break
 				except Exception as e:
