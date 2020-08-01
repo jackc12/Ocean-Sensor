55c7f8ff1ce894459b433cdb1fd04a8e971690cf
J C <jc@Js-MacBook-Pro.fios-router.home>
2020-07-31 20:26:37 -0400

    new commit

--- sensor.py
+++ sensor.py
@@ -83,7 +83,6 @@ class ConductivitySensor(Sensor):
 
 class OxygenSensor(Sensor):
 	def do_sample(self, n_samples=6, interval=5, wait_for=10):
-		print('OK')
 		self.n_samples = n_samples
 		self.written_samples = 0
 		self.wait_for = wait_for
@@ -93,7 +92,6 @@ class OxygenSensor(Sensor):
 		failed_conductivity = True
 		while time.time() <= end_at and self.written_samples < self.n_samples:
 			while 'Oxygen:' not in sat_and_temp and 'Saturation:' not in sat_and_temp and 'Temperature:' not in sat_and_temp:
-				print(sat_and_temp)
 				try:
 					self.ser.flushInput()
 					self.ser.write(bytes('do sample','utf-8'))
@@ -111,8 +109,7 @@ class OxygenSensor(Sensor):
 				print('wrote to error.txt! error in Conductivity.get_sample!')
 				quit()
 			else:
-				print(len(sat_and_temp.split()))
-				if len(sat_and_temp.split()) != 3:
+				if len(sat_and_temp.split()) == 3:
 					write_file(f_name='oxygen.txt', msg='{} Oxygen: {} Saturation: {} Temperature: {}\n'.format(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), sat_and_temp.split()[0], sat_and_temp.split()[1], sat_and_temp.split()[2]))
 					print('{} Oxygen: {} Saturation: {} Temperature: {}\n'.format(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), sat_and_temp.split()[0], sat_and_temp.split()[1], sat_and_temp.split()[2]))
 					self.written_samples += 1
