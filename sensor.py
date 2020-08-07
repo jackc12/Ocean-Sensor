a594cf08903431f1dbc4d33138b022d0ee520b4c
J C <jc@Js-MacBook-Pro.fios-router.home>
2020-08-07 14:57:40 -0400

    commit

--- sensor.py
+++ sensor.py
@@ -1,7 +1,7 @@
 import serial, time, datetime
 
 def write_file(f_name='error.txt', msg='you didn\'t pass any arguments to write_file'):
-	with open('~/Ocean-Sensor/' + f_name, 'ca') as f:
+	with open('~/Ocean-Sensor/' + f_name, 'a') as f:
 		f.write(msg)
 
 class Sensor:
